f16f3334f3c3e22c3bd12e6be5fb721ea310c301
Ilya Komar <helias@yandex.ru>
2021-01-07 16:39:14 +0300

    feat: add html of profile settings and slide right animation

--- src/js/modules/initToggleProfile.js
+++ src/js/modules/initToggleProfile.js
@@ -1,10 +1,13 @@
 export default () => {
   const profileHeader = document.querySelector('.profile-header');
   const profileLinkBack = document.querySelector('.profile-header__arrow');
+  const profileSettings = document.querySelector('.inner__col1-profile');
+
   profileHeader.addEventListener('click', (e) => {
     // show/hide arrow in profile header on desktop
     if (e.target.closest('.profile-header__wrap, .profile-header__arrow')) {
       profileLinkBack.classList.toggle('profile-header__arrow_visible');
+      profileSettings.classList.toggle('inner__col1-profile_active');
     }
   });
 };
