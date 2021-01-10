4bb0b97d6e59a904c4f81720e583b7ba541cd25f
Fr3ud <oo.pavelko@gmail.com>
2021-01-10 14:32:38 +0200

    feat: add swiper js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+import 'swiper/swiper-bundle.css';
 import './styles/index.scss';
+import Swiper from 'swiper';
+
+const mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+})
