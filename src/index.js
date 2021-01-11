28b99b9b53b1b43f511d12f83ffcaef6b5f25ca7
Fr3ud <oo.pavelko@gmail.com>
2021-01-11 16:34:40 +0200

    feat: add authorization popup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import 'swiper/swiper-bundle.css';
 import './styles/index.scss';
 import Swiper from 'swiper';
+import MicroModal from 'micromodal';
 
 const mySwiper = new Swiper('.swiper-container', {
     loop: true,
     slidesPerView: 3,
     spaceBetween: 20,
-})
+});
+
+MicroModal.init({
+    disableScroll: true,
+    awaitOpenAnimation: true,
+    awaitCloseAnimation: true,
+});
