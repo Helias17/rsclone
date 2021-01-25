9e18ac1ecd0653cb385296bfc6c370d657524e7d
Fr3ud <oo.pavelko@gmail.com>
2021-01-25 13:25:05 +0200

    feat: set responsive breakpoints for slider

--- src/js/modules/initPlugins.js
+++ src/js/modules/initPlugins.js
@@ -1,14 +1,29 @@
 import Swiper from 'swiper';
 import MicroModal from 'micromodal';
 
+const closeModals = (modal) => {
+  // const modalsId = ['modal-', 2, 3];
+  // modalsId.filter((id) => id !== modal.id).forEach((id) => {
+  //   console.log(id);
+  //   MicroModal.close(`modal-${id}`);
+  // });
+  console.info(`${modal.id} is shown`);
+};
+
 const initPlugins = () => {
   const mySwiper = new Swiper('.swiper-container', {
     loop: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 20,
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      },
+    },
   });
 
   MicroModal.init({
+    onShow: closeModals,
     disableScroll: true,
     awaitOpenAnimation: true,
     awaitCloseAnimation: true,
